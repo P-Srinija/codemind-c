92f16b687dded53c9c2fbc799826d233516217e0
P-Srinija <147487819+P-Srinija@users.noreply.github.com>
2023-10-18 19:37:16 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d",&x,&y);     if(x%2==0&&y%2==0)     printf("YES");     else if(x==0&&y%2!=0)     printf("NO");     else if((x+(2*y))%2==0)     printf("YES");     else     printf("NO");
    }

---
+++ Can_share_equally?.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d",&x,&y);
+    if(x%2==0&&y%2==0)
+    printf("YES");
+    else if(x==0&&y%2!=0)
+    printf("NO");
+    else if((x+(2*y))%2==0)
+    printf("YES");
+    else
+    printf("NO");
+    
+}
