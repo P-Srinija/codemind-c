3556c759438469fd209cac621ec48adba5296f7e
P-Srinija <147487819+P-Srinija@users.noreply.github.com>
2023-12-19 23:18:00 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     int t=1;     for(i=n;i>=1;i--)     {         for(j=i;j>=1;j--)         {             printf("%d ",t);         }         t++;         printf(" ");     } }

---
+++ Basic_Pattern_3.2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    int t=1;
+    for(i=n;i>=1;i--)
+    {
+        for(j=i;j>=1;j--)
+        {
+            printf("%d ",t);
+        }
+        t++;
+        printf("
+");
+    }
+}
