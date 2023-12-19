05d6a9dfee4ff9c0d6d069a757306d08e0bb8a6e
P-Srinija <147487819+P-Srinija@users.noreply.github.com>
2023-12-19 14:54:17 +0530

    #include<stdio.h>
    int main()
    {
        int a,n,i,s=0,c=0;
        scanf("%d",&n);
        int arr[n];
        for(i=0;i<n;i++)
        {
            scanf("%d",&arr[i]);
            s=s+arr[i];
        }
        a=s/n;
        for(i=0;i<n;i++)
        {
            if(arr[i]<=a)
            c=c+1;
            
        }
        printf("%d",c);
        
    }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,n,i,s=0,c=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        s=s+arr[i];
+    }
+    a=s/n;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<=a)
+        c=c+1;
+        
+    }
+    printf("%d",c);
+    
+}
