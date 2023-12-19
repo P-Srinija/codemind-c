bdf283169b713c2240936cb088ca85f4945c94e2
P-Srinija <147487819+P-Srinija@users.noreply.github.com>
2023-12-19 20:28:54 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     int arr[n];     int ar[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     j=0;     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             ar[j]=arr[i];             j++;         }     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             ar[j]=arr[i];             j++;         }     }     for(i=0;i<n;i++)     {         printf("%d ",ar[i]);     } }

---
+++ Even_elements_followed_by_Odd_elements.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    int arr[n];
+    int ar[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    j=0;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            ar[j]=arr[i];
+            j++;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            ar[j]=arr[i];
+            j++;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",ar[i]);
+    }
+}
