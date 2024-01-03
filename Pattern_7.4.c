1fff7606f34e31b32063a67342922845c1887d57
P-Srinija <147487819+P-Srinija@users.noreply.github.com>
2024-01-03 17:44:44 +0530

    #include<stdio.h> int main() { 	int i,j,n,k,l,m; 	scanf("%d",&n); 	l=n; 	for(i=0;i<n;i++) 	{ 		for(j=0;j<i;j++) 		{ 			if(j<i) 			printf(" "); 		} 		for(k=0;k<l;k++) 		{ 			printf("%c ",64+l); 		} 		l--; 		printf(" "); 	} }

---
+++ Pattern_7.4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n,k,l,m;
+	scanf("%d",&n);
+	l=n;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<i;j++)
+		{
+			if(j<i)
+			printf(" ");
+		}
+		for(k=0;k<l;k++)
+		{
+			printf("%c ",64+l);
+		}
+		l--;
+		printf("
+");
+	}
+}
